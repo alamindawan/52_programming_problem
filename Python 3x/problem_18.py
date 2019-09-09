0fdd0338c28581ae6f841688fa1d8884a6fa7292
Alamin <alamin@sizramsolutions.com>
2019-09-09 07:14:44 +0600

    18 no problem solved using python 3

--- Python 3x/problem_18.py
+++ Python 3x/problem_18.py
@@ -1,32 +1,18 @@
-#this is not accept but i could not find any error
-vowel = ['a','e','i','o','u']
-num = input()
-for n in range(int(num)):
-    strVowel = strCons = ''
-    inputData = input()
-    numList = inputData.replace(" ","")
-    for x in numList:
-        if x.lower() in vowel:
-            strVowel += x
-        else:
-            strCons += x
 
-    print("{}".format(strVowel))
-    print("{}".format(strCons))
 
 #accept this solution
 num = int(input())
 
 for x in range(num):
-    textInput = input().replace(" ", "")
+    anotherInput = input().replace(" ", "")
     vowelLetter = 'aeiou'
     consLetter = 'bcdfghjklmnpqrstvwxyz'
     vowels = ''
     consonent = ''
-    for n in textInput:
-        if j.lower() in vowelLetter:
+    for n in anotherInput:
+        if n.lower() in vowelLetter:
             vowels += n
-        elif j.lower() in consLetter:
+        elif n.lower() in consLetter:
             consonent += n
     print("{}".format(vowels))
     print("{}".format(consonent))
